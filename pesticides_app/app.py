e90a0fec697e92cfea7f8cff8f959fb645debefb
mavbib <mavquart@gmail.com>
2023-04-13 18:44:19 -0400

    9th commit correct assets-static file bug

--- pesticides_app/app.py
+++ pesticides_app/app.py
@@ -27,26 +27,33 @@ area_name_dict = {l: l for l in area_names}
 
 app_ui = ui.page_fluid(
     ui.tags.head(
-        ui.tags.link(rel="stylesheet",
-                     href="{static_content_url}/styles.css"),
-        ui.tags.div(class_="input_container")),
-    ui.panel_title("Pesticides Usage by Country"),
-    ui.layout_sidebar(
 
-        ui.panel_sidebar(ui.input_selectize(id="Area", label="Areas",
-                                            choices=area_name_dict, selected="Net Food Importing Developing Countries", multiple=True),
-                         ui.input_date_range(id="date_range", label="Date Range", start=date_range_start,
-                                             end=date_range_end),
+        ui.tags.link(rel="stylesheet", href="{static_content_url}/styles.css")),
+    ui.tags.div(style="color: #444444;", children=[
 
-                         ),
-        ui.panel_main(ui.output_plot("plottimeseries")
-                      ),
+        ui.panel_title("Pesticides Usage by Country")
+    ]),
+    ui.tags.body(
+        ui.tags.div(style="background-color: #ffb265;", children=[
+            ui.layout_sidebar(
 
-    ),
-    ui.tags.p(
-        "The data are from Food and Agriculture Organisation of the United Nations. Some figures are official and some are estimated."),
-    ui.a("FAO.org", href="https://www.fao.org/home/en"),
-    ui.tags.p("Please note: adding country will change the scale of metric tonnes.")
+                ui.panel_sidebar(ui.input_selectize(id="Area", label="Areas",
+                                                    choices=area_name_dict, selected="Net Food Importing Developing Countries", multiple=True),
+                                 ui.input_date_range(id="date_range", label="Date Range", start=date_range_start,
+                                                     end=date_range_end),
+
+                                 ),
+
+                ui.panel_main(ui.output_plot("plottimeseries")
+                              ),
+
+            )]),
+        ui.tags.p(
+            "The data are from Food and Agriculture Organisation of the United Nations. Some figures are official and some are estimated"),
+        ui.a("FAO.org", href="https://www.fao.org/home/en"),
+        ui.tags.p(
+            "Please note: adding country will change the scale of metric tonnes.")
+    )
 )
 
 
